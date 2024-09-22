36327a9d55d39f1be323bdafb4f9ae6093f2cd54
Douglas Little <douglas.a.little@gmail.com>
2024-09-22 18:42:42 -0400

    Final

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,15 @@ namespace MS539_GUI_App
         {
             InitializeComponent();
         }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
